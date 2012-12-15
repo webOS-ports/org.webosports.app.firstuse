3fe6bcdfd3db18876e84c1e218a6c3fd31d7ed65
josh <jpalmerwatkins@me.com>
2012-12-15 19:36:51 +0000

    First Use: Add animations
    Open-WebOS-DCO-1.0-Signed-Off-By: Josh Palmer <jpalmerwatkins@me.com>

--- source/App.js
+++ source/App.js
@@ -2,7 +2,9 @@ enyo.kind({
 	name: "App",
 	classes: "enyo-fit",
 	layoutKind: "FittableRowsLayout",style: "padding: 8px",
+	licenseAccepted: false,
 	components:[
+		{name: "OpacityAnimator", kind: "Animator", startValue: 1, endValue: 0, duration: 1000, onStep: "animatorStep", onEnd: "animatorEnd"},
 		{kind: "Scroller", fit: true, touch: true, horizontal: "hidden", components:[
 			{kind: "PortsHeader",
 			title: "License",
@@ -17,17 +19,41 @@ enyo.kind({
 			{content: "LICENSE PLACEHOLDER<br>You (the user) hereby agree to be generally apathetic about this piece of text (the piece of text).", allowHtml: true, style: "padding: 10px; color: white;"}
 		]},
 		{tag: "div", style: "margin: 8px 8% 0 8%; padding: 0; line-height: 42px;", layoutKind: "FittableColumnsLayout", components:[
-			{kind: "onyx.Button", style: "width: 45%; color: white; background-color: darkred;", content: "Decline"},
+			{kind: "onyx.Button", style: "width: 45%; color: white; background-color: darkred;", content: "Decline", ontap: "declineLicense"},
 			{fit: true},
 			{kind: "onyx.Button", style: "width: 45%; color: white; background-color: green;", content: "Accept", ontap: "acceptLicense"}
 		]}
 	],
+	rendered: function(inSender, inEvent) {
+		this.inherited(arguments);
+		this.$.OpacityAnimator.setStartValue(0);
+		this.$.OpacityAnimator.setEndValue(1);
+		this.$.OpacityAnimator.play();
+	},
 	acceptLicense: function(inSender, inEvent) {
-		/* this will create the needed /var/luna/preferences/ran-first-use file to start
-		 * LunaSysMgr in lunaui mode */
-		PalmSystem.markFirstUseDone();
-		/* NOTE: this will trigger a restart of the LunaSysMgr as it exits and upstart
-		 * will restart it immediately */
-		PalmSystem.shutdown();
+		this.licenseAccepted = true;
+		this.$.OpacityAnimator.setStartValue(1);
+		this.$.OpacityAnimator.setEndValue(0);
+		this.$.OpacityAnimator.play();
+	},
+	declineLicense: function(inSender, inEvent) {
+		this.$.OpacityAnimator.setStartValue(1);
+		this.$.OpacityAnimator.setEndValue(0);
+		this.$.OpacityAnimator.play();
+	},
+	animatorStep: function(inSender, inEvent) {
+		enyo.Arranger.opacifyControl(this, inSender.value);
+	},
+	animatorEnd: function(inSender, inEvent) {
+		if(this.licenseAccepted == true) {
+			/* this will create the needed /var/luna/preferences/ran-first-use file to start
+			 * LunaSysMgr in lunaui mode */
+			PalmSystem.markFirstUseDone();
+		}
+		
+		if(this.$.OpacityAnimator.endValue == 0) {
+			//This should shutdown the device, but for now just restarts firstuse luna again
+			PalmSystem.shutdown();
+		}
 	}
 });
