35825a101aebfc912f9102e758ab6c5f7e9f66f7
Simon Busch <morphis@gravedo.de>
2013-03-25 17:00:05 +0100

    Some small adjustment to license text
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- source/License.js
+++ source/License.js
@@ -4,8 +4,9 @@ It is provided without warranty, even the implied warranty of merchantability, \
 satisfaction or fitness for a particular use. See the licence included with each program for details.<br>\
 <br>\
 Some licences may grant additional rights; \
-this notice shall not limit your rights under each program's licence. Licences for each program are available in the usr/share/doc directory. \
-Source code for Open webOS (Ports) can be downloaded from github.com/webOS-ports.<br>\
+this notice shall not limit your rights under each program's licence. Licences for each program are available in the /usr/share/licenses directory. \
+Source code for Open webOS (Ports) can be downloaded from http://github.com/openwebos and \
+http://github.com/webOS-ports.<br>\
 <br>\
 Open webOS (Ports) is released for limited use due to the inclusion of binary hardware support files. \
 The original components and licenses can be found at:<br>\
